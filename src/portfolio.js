5bd9fbbe8493e888b4715ea89bf8ba35ecfcc280
Mayuri Warale <97494267+mayuri3598@users.noreply.github.com>
2022-04-29 11:32:32 +0530

    Add files via upload

---
+++ src/portfolio.js
@@ -0,0 +1,174 @@
+//I Would Highly Recommend You To Customize The Theme According To Your Taste.
+export const globalStyles = {
+  "--grad": "linear-gradient(315deg,#48a9fe,#00cdac)", // Use gradients only
+  // "--grad": "linear-gradient(315deg, #48a9fe 0%, #734ae8 74%)", // Use gradients only
+  // "--grad": "linear-gradient(315deg, #06bcfb 0%, #4884ee 74%)",
+  "--txtcolor": "#272341",
+  "--p_color": "#00cdac",
+  "--s_color": "#fff",
+};
+
+// Navigation Bar Name
+export const navbar_name = "<Mayuri>";
+
+// Main Page
+export const main = {
+  name: "Mayuri Nilkanth Warale",
+  doamin: "A MERN Stack Web Developer",
+  resume:
+    "https://drive.google.com/file/d/18o2BPOWxHSRv9uosIqxgm9vpQTebgrC4/view?usp=sharing",
+};
+
+// AboutMe
+export const aboutme = {
+  imgLink:
+    "https://avatars.githubusercontent.com/u/97494267?s=400&u=6355fe247219c4d4ff271db7735752a81e30007e&v=4",
+  p1: "Hey, My name is Mayuri Nilkath Warale. I have completed my Bachelor in Electrical from Matoshri College of Engineering, Nanded(Maharashtra).",
+  p2: " I'm a passionate coder and an enthusiastic Web Developer capable of applying programming skills to solve complex problems. Seeking a challenging role in the field of software development that will offer an opportunity for further development of my abilities, skills and knowledge in an organization.",
+  p3: "Apart from coding I also like Fitness, cooking , read books and explore new things.",
+};
+
+// Skills
+export const whatido = {
+  title: "Full Stack Development",
+
+  details: [
+    "Building responsive website using HTML,CSS,JS,React",
+    "Creating backend application in Node, ExpressJS",
+    "Building Simple RESTful CRUD APIs",
+    "Knowledge of MongoDB Database",
+    "Knowledge of various UI libraries",
+  ],
+  logos: [
+    {
+      name: "HTML5",
+      iconifyClassName: "vscode-icons:file-type-html", // PS : Use iconify class name
+      id: "1",
+    },
+    {
+      name: "CSS",
+      iconifyClassName: "vscode-icons:file-type-css",
+      id: "2",
+    },
+    {
+      name: "JavaScript",
+      iconifyClassName: "logos:javascript",
+      id: "3",
+    },
+    {
+      name: "ReactJS",
+      iconifyClassName: "vscode-icons:file-type-reactjs",
+      id: "4",
+    },
+    {
+      name: "Redux",
+      iconifyClassName: "logos:redux",
+      id: "5",
+    },
+    {
+      name: "NodeJS",
+      iconifyClassName: "logos:nodejs",
+      id: "6",
+    },
+    {
+      name: "ExpressJS",
+      iconifyClassName: "logos:express",
+      id: "7",
+    },
+    {
+      name: "MongoDB",
+      iconifyClassName: "logos:mongodb",
+      id: "8",
+    },
+    {
+      name: "Postman",
+      iconifyClassName: "logos:postman-icon",
+      id: "9",
+    },
+    {
+      name: "npm",
+      iconifyClassName: "logos:npm-icon",
+      id: "10",
+    },
+    {
+      name: "GIT",
+      iconifyClassName: "logos:git",
+      id: "11",
+    },
+    {
+      name: "Tailwind",
+      iconifyClassName: "logos:tailwindcss-icon",
+      id: "12",
+    },
+    {
+      name: "Material-UI",
+      iconifyClassName: "logos:material-ui",
+      id: "13",
+    },
+    {
+      name: "Ant Design",
+      iconifyClassName: "logos:ant-design",
+      id: "14",
+    },
+    {
+      name: "Netlify",
+      iconifyClassName: "vscode-icons:file-type-netlify",
+      id: "15",
+    },
+    {
+      name: "Vercel",
+      iconifyClassName: "logos:vercel-icon",
+      id: "16",
+    },
+  ],
+};
+
+// Project Details
+export const details = [
+  {
+    id: 1,
+    title: "Dot & Key E-commerce website - clone",
+    desc: `Dot&Key is an e-commerce website for beauty products.`,
+    feature: `Sign-in/Sign-up validation with backend,
+    corousel image,
+Dynamic product page,
+payment gateway,Dynamic cart page with coupon functionality.`,
+    tech: `Tech Stack: EJS | CSS | JavaScript | nodeJS | express | MongoDB`,
+    img: "https://cdn.grabon.in/gograbon/images/web-images/uploads/1618488952053/dot-and-key-coupons.jpg",
+    github: "https://github.com/0Raj/DotAndKey",
+    link: "https://dontandkey.netlify.app/"
+  },
+  {
+    id: 2,
+    title: "KFC E-commerce website / Clone",
+    desc: `KFC is an online food ordering website for fried chicken, burgers and beverages.`,
+    feature: `Sign-in/Sign-up,
+search bar for food items to see their nutrition contents, explore various fitness tracking apps,
+Subscription page.`,
+    tech: `Tech Stack: HTML | CSS | JavaScript `,
+    img: "https://miro.medium.com/max/1400/1*KhA5_iWUN02BY33xEluVxQ.jpeg",
+    link: "https://sage-dolphin-365e39.netlify.app/index.html",
+    github: "https://github.com/salove16/KFC-COMPLETE-PROJECT.git",
+  },
+  
+//   {
+//     id: 6,
+//     title: "My Recipe App",
+//     desc: `It's a food recipe app, users can search recipes of various dishes and also see the latest recipes as well as the recipe of the day.`,
+//     feature: `Search recipe,
+// Recipe of the day.`,
+//     tech: `Tech Stack: HTML | CSS | JavaScript`,
+//     img: "https://github.com/Shivam2101s/images/blob/main/food%20app.jpg?raw=true",
+//     link: "https://myfoodapp21.netlify.app/",
+//     github: "https://github.com/ashish1234sharma/Food-app",
+//   },
+];
+
+// Social Media Links
+export const links = [
+  
+  "https://www.linkedin.com/in/mayuri-warale/",
+  // "https://api.whatsapp.com/send/?phone=918109560498&text&app_absent=0",
+  "mailto:mayuriwaralepatil@gmail.com",
+  "https://github.com/mayuri3598",
+];
